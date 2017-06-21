667db71be1d2ec53f72fb3b19a7a972c74360332
Santi Scalzadonna <pilujander@gmail.com>
2017-06-21 21:39:03 +0200

    Proposed solution

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,47 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This is a proposed solution to Assingment2
+## where lexical scope is used to calculate and
+## store in cache the inverse of a matrix
 
-## Write a short comment describing this function
+## Stores a matrix and its inverse
+## Provides getter and setter methods:
+## x is the matrix
+## inv is the result of the inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) inv <<- inverse
+  getinverse <- function() inv
+  list(set = set, get = get,
+    setinverse = setinverse,
+    getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## This function takes a matrix as an argument
+## and returns its inverse, using makeCacheMatrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinverse()
+  if(!is.null(inv)) {
+    message("getting cached inverse matrix")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data, ...)
+  x$setinverse(inv)
+  inv
 }
+
+##usage: first create a matrix, call it mtx
+##then call makeCacheMatrix(mtx) 
+##with the created matrix as parameter (mtx)
+##assing the result to a new variable: mc
+##the call cacheSolve(mc)
+##with mc the result from makeCacheMatrix
+## call a second time cacheSolve(mc)
+##you'll get the cached results :)
